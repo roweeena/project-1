0584179ac831ea80032879c726d69043f13b6008
roweeena <roe0320@gmail.com>
2021-07-26 14:10:01 +1000

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+  get 'cart/show'
   root :to => "pages#home"
   resources :users
   resources :items
-  resource :cart, only: [:show] do
-    put 'add/:item_id', to: 'carts#add', as: :add_to
-    put 'remove/:product_id', to: 'carts#remove', as: :remove_from
-  end
+  resource :cart
 
   #not crud #controller#action
   get '/login' => 'sessions#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_012618) do
+ActiveRecord::Schema.define(version: 2021_07_26_034058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.text "image"
