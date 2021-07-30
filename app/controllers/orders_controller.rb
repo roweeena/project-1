009c2cd9002a4ca01b2cbb614cc93585f1696230
roweeena <roe0320@gmail.com>
2021-07-30 13:10:25 +1000

    last minute change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,12 +10,12 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new order_params
     @current_cart.line_items.each do |item|
-      @order.line_items << item
+      @order.line_items << item #append each item as line item in the order
       item.cart_id = nil
     end
     @order.save
-    Cart.destroy(session[:cart_id])
-    session[:cart_id] = nil
+    Cart.destroy session[:cart_id]
+    session[:cart_id] = nil #destroy the cart and set the session back to nil
     flash[:notice] = 'Purchase successful'
     redirect_to root_path
   end
